57df30b3beb82f0c7fab545447e89758f67db0eb
galiyafatt2 <elizabeth.mor324@gmail.com>
2022-10-13 03:03:27 +0300

    m

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,7 +5,7 @@ class Room < ApplicationRecord
   has_many_attached :images
 
   def images_as_thumbnails(image)
-    return unless image&.content_type.in?(%w[image/jpg image/jpeg image/png])
+    return unless image.content_type.in?(%w[image/jpg image/jpeg image/png])
 
     image.variant(resize_to_limit: [400, 200]).processed
   end
