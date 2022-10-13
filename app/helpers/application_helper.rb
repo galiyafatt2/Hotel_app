8e1625bef95dfba1eed0dcbce1415707d7e23756
galiyafatt2 <elizabeth.mor324@gmail.com>
2022-10-13 21:15:26 +0300

    m

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def image_tag(source, options={})
-    source = 'assets/images/blank.jpg' unless super(source, options)
+    source = 'assets/images/blank.jpg' if source.blank?
     super(source, options)
   end
 end
