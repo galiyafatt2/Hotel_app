f3e3b79f6e44991848daa8f3fb62b291950c29c8
galiyafatt2 <elizabeth.mor324@gmail.com>
2022-10-14 10:49:37 +0300

    m

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def image_tag(source, options={})
-    source = 'assets/images/blank.jpg' if source.blank?
-    super(source, options)
-  end
 end
