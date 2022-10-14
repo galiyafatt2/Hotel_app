8ced1f601b7c5e1c337d68e6d5a60b39e18979f4
galiyafatt2 <elizabeth.mor324@gmail.com>
2022-10-14 11:14:09 +0300

    m

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Hotel
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_job.queue_adapter = :sidekiq
+    config.public_file_server.enabled = true
 
     # Configuration for the application, engines, and railties goes here.
     #
